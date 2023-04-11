e92430b7880b639bccb723bf949fe6ba38c9b19a
naimurk <naimurk781@gmail.com>
2023-04-12 00:38:26 +0600

    ok

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import { addToDb } from '../../localStorage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
@@ -37,7 +37,8 @@ const Header = () => {
                             </span>
                         </div>
                         <ul className={`md:flex gap-x-8 absolute duration-500 lg:p-3 md:static ${isOpen ? 'top-20 right-3 w-1/2 bg-stone-200 ' : '-top-40 right-3'}`}>
-                            <Link to='/'><li className='mb-1 p-2  hover:bg-purple-400 lg:p-0 lg:mb-0' >Home</li></Link>
+                            {/* <NavLink to ='/' activeClassName='text-purple-400 !important'   ><li className='mb-1 p-2  hover:bg-purple-400 lg:p-0 lg:mb-0' >Home</li></NavLink> */}
+                            <Link to="/"><li className='mb-1 p-2  hover:bg-purple-400 lg:p-0 lg:mb-0'>Home</li></Link>
                             <Link to="/statics"><li className='mb-1 p-2  hover:bg-purple-400 lg:p-0 lg:mb-0'>Statistics</li></Link>
                             <Link to="/applied_job" ><li className=' p-2  hover:bg-purple-400 lg:p-0 lg:mb-0'>Applied job</li></Link>
                             <Link><li className=' p-2  hover:bg-purple-400 lg:p-0 lg:mb-0'>Blog</li></Link>
