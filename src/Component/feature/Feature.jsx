e245b02a86aceaaaab308982dce24e275d9d35cc
naimurk <naimurk781@gmail.com>
2023-04-11 16:12:27 +0600

    chekc

--- src/Component/feature/Feature.jsx
+++ src/Component/feature/Feature.jsx
@@ -4,29 +4,35 @@ import SingleFeature from './SingleFeature';
 
 
 const Feature = () => {
-    const [Feature, setfeature] = useState([])
-    useEffect(()=>{
-        fetch('feature.json')
-        .then(res=>res.json())
-        .then(data => setfeature(data))
-    },[])
+        const [Feature, setfeature] = useState([])
+
+        useEffect(() => {
+        fetch('/feature.json')
+            .then(res => res.json())
+            .then(data => setfeature(data))
+    }, [])
+    
+    
+
+
     // console.log(Feature);
     // const sayem = useContext(Contex)
-    
+
     return (
-        <div>
-            <div className='py-28 text-center'> 
+        <div className='py-12'>
+            <div className='py-28 text-center'>
                 <h1 className='text-4xl font-bold'>Featured Jobs</h1>
                 <p className='mt-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className='grid grid-cols-2 justify-center gap-12'>
-                 {
+                {
                     Feature.map(sF => <SingleFeature
-                    key={sF.id}
-                    sF = {sF}
+                        key={sF.id}
+                        sF={sF}
                     ></SingleFeature>)
-                 }
+                }
             </div>
+            <button className='px-12 py-5 text-white bg-black mx-auto' >see all</button>
         </div>
     );
 };
